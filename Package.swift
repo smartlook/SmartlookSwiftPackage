3f589241dc4cda46e7637064a77e0d93ca6183f8
Pavel Kroh <44899602+pavel-kroh-smartlook@users.noreply.github.com>
2020-12-07 15:47:29 +0100

    Version 1.7.0 (#1)

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Smartlook",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "Smartlook",
+            targets: ["Smartlook"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Smartlook",
+            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-1.7.0.3014.zip",
+            checksum: "8996be5d5401d7dad13ccbccaef2b982edcdf2d656363db555980c04c6ea5a0e"
+        ),
+    ]
+)                                       
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# swift-package
-Swift package for Smartlook iOS SDK
+# Smartlook Swift Package
+
+The official Swift package for Smartlook iOS SDK, a mobile analytics solution.
+
+See also:
+- about [Smartlook](https://smartlook.com)
+- [Documentation](https://smartlook.github.io)
+
+- [Issue tracking](https://github.com/smartlook/smartlook-mobile-issue-tracker/issues)
