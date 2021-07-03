21f75d8659011b1f6d40be4a2d73571979ed8305
Pavel Kroh <pavel.kroh@gmail.com>
2021-07-03 10:40:00 +0200

    Fix framework download URL

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Smartlook",
-            url: "https:///sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.7.6.1.3350.zip",
+            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.7.6.1.3350.zip",
             checksum: "7698f855efa4d1bf68198205fc949e05f4cfe3b4af19ad7bb5afb1e1db07ce68"
         ),
     ]
