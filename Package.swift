b14623b8a7785057e8e1987f30132dd18bbaf96b
Pavel Kroh <pavel.kroh@gmail.com>
2021-10-19 12:24:17 +0200

    Pre-release 1.8.0.6

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Smartlook",
-            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.8.0.5.zip",
-            checksum: "ad2ac6340fb51777509a372e44d8e51ad27b04559b09744f019417bf85c492b0"
+            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.8.0.6.zip",
+            checksum: "0b36d1360bb0bc2dcd5c0e7421e3b055986f8e7e30a8ebfcefd76580abbedc21"
         ),
     ]
 )
