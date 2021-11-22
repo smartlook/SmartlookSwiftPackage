e2768dbf579d2707fefa7550f7852ac4aeefecd0
Pavel Kroh <pavelkroh@smartlook.com>
2021-11-22 17:14:28 +0100

    Beta 1.8.2.37 / bitcode symbols

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Smartlook",
-            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.8.2.37.zip",
-            checksum: "13db26ffed03368c44d90c0add1c2af33338c679eb8dc3c312fed06286b81763"
+            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.8.2.39.zip",
+            checksum: "ca857a35e6f41f9d56e59962b86ff1fba5831289a4427a5b06833feccf62b9c5"
         ),
     ]
 )
