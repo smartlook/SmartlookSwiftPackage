34c40df37f758dcf085db54e6ee4d277824f6ce7
Pavel Kroh <pavelkroh@smartlook.com>
2021-11-22 18:17:19 +0100

    Beta 1.8.2.41 dSYM included

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Smartlook",
-            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.8.2.39.zip",
-            checksum: "ca857a35e6f41f9d56e59962b86ff1fba5831289a4427a5b06833feccf62b9c5"
+            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.8.2.41.zip",
+            checksum: "ff0c65bfd3170b5ff7da176834e6d083780617d3dbfebbd14259fdddb94c1cfa"
         ),
     ]
 )
