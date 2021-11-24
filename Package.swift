149bfca09ba48feab014c2265c8d30bdf212e377
Pavel Kroh <pavelkroh@smartlook.com>
2021-11-24 12:18:06 +0100

    Build 42

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Smartlook",
-            url: "https://sdk.smartlook.com/ios/beta/smartlook-ios-sdk-1.8.2.41.zip",
-            checksum: "ff0c65bfd3170b5ff7da176834e6d083780617d3dbfebbd14259fdddb94c1cfa"
+            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-1.8.2.42.zip",
+            checksum: "7ff84c5ab71d744b33627402dfdfd92ff1b8c4d8a77b117816d52e6cb46cc088"
         ),
     ]
 )
