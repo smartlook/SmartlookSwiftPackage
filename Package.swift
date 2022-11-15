3b0b508f913fa7b586a4eb671afaca849c9e27e8
Pavel Kroh <pavelkroh@smartlook.com>
2022-11-15 10:49:25 +0100

    Block Catalyst recording

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Smartlook",
-            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-1.9.2.162.zip",
-            checksum: "db4ab30d5749c7ba72b88da2b166d5f645acbae43ba7e4fb9bc7460efeaa79f0"
+            url: "https://sdk.smartlook.com/ios/smartlook-ios-sdk-1.9.2.164.zip",
+            checksum: "00981007155e9d2dc5d7fe99db34abf90efafce8a627694e9e02b48c495c7405"
         ),
     ]
 )
